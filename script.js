e32e76808271257038576e5ababa7d43c01dcd68
yoksel <yoksel@yandex.ru>
2013-12-30 00:47:48 +0400

    Add Unicode blocks

--- script.js
+++ script.js
@@ -1,11 +1,21 @@
 var step = 10000;
+var title_symbols = document.querySelector(".title--symbols");
 
 print_select();
 
-print_symbols ( 10000 );
+print_symbols ( 0, step, "page" );
+var text = "0  - " + num_readable( step );
+title_symbols.innerHTML = text;
+
+add_action_to_rangelist ()
+
 
 function num_to_hex ( num ){
     return num.toString(16);
+}
+
+function hex_to_num ( hex ){
+    return parseInt(hex, 16);
 }
 
 function num_readable( num ) {
@@ -27,27 +37,33 @@ function print_select(){
         options += "<option value=\"" + step_max + "\">" + step_output + "</option>"
     }
 
-    options = "Диапазон: <select id=\"range_select\" onchange=\"print_symbols()\">" + options + "</select>"; 
+    options = "Диапазон: <select id=\"range_select\">" + options + "</select>"; //onchange=\"print_symbols()\"
 
     placeOutput.innerHTML = options;
 
+    var myselect = document.getElementById("range_select");
+    myselect.addEventListener('change', function() {
+            var select_val = this.options[this.selectedIndex].value;
+            var max = +select_val;
+            var min = max - step + 1;
+            var text = num_readable( min ) + " - " + num_readable( max );
+
+            title_symbols.innerHTML = text;
+            print_symbols ( min, max );
+            remove_current();
+            }, false);
+
 }
 
-function print_symbols ( max ) {
+function print_symbols ( min, max, context ) {
+    var out = "";
 
     var placeOutput = document.getElementById("symbols");
     placeOutput.innerHTML = "Загрузка";
 
-    var myselect = document.getElementById("range_select");
-    var select_val = myselect.options[myselect.selectedIndex].value;
+    min = min || max - step + 1;
 
-    
-    max = max || select_val;
-
-    var out = "";
-    var min = max - step + 1;
-    
-    out += "<ul>";
+    out += "<ul class=\"list--entities\">";
 
     for ( var i = min; i <= max; i++ ){
     	var char_i = String.fromCharCode(i);
@@ -58,11 +74,40 @@ function print_symbols ( max ) {
 
     	out += ( i < max 
                  && i > 0 
-                 && i % 1000 == 0 ) ? "</ul>\n<h2>" + i + "</h2><ul>" : "";
+                 && i % 1000 == 0 ) ? "</ul>\n<h2>" + i + "</h2><ul class=\"list--entities\">" : "";
     }
 
     out += "</ul>";
 
     placeOutput.innerHTML = out;
 
+}
+
+function remove_current() {
+    var current_item = document.querySelector(".current");
+
+    if ( current_item ) {
+        current_item.classList.remove('current');
+        }
+}
+
+function add_action_to_rangelist () {
+    var separator = ".."
+    var list_items = document.querySelectorAll(".list--blocks LI");
+    
+    for ( var i = 0; i < list_items.length; i++ ){
+        var item = list_items[i];
+
+        item.addEventListener('click', function() {
+            var range = this.getAttribute('data-range').split(separator);
+            var min = hex_to_num( range[0] );
+            var max = hex_to_num( range[1] );
+            var text = this.textContent;
+
+            print_symbols ( min, max, "range" );
+            title_symbols.innerHTML = text;
+            remove_current();
+            this.classList.add('current');
+            }, false);
+    }
 }
