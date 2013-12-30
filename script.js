27ae96861f9fa7293746d0f004993215d389c3e5
yoksel <yoksel@yandex.ru>
2013-12-30 23:56:11 +0400

    Add print list from url. Add list of favorites blocks.

--- script.js
+++ script.js
@@ -1,14 +1,9 @@
+var separator = "..";
 var step = 10000;
 var title_symbols = document.querySelector(".title--symbols");
 
-print_select();
 
-print_symbols ( 0, step, "page" );
-var text = "0  - " + num_readable( step );
-title_symbols.innerHTML = text;
-
-add_action_to_rangelist ()
-
+initial();
 
 function num_to_hex ( num ){
     return num.toString(16);
@@ -21,6 +16,24 @@ function hex_to_num ( hex ){
 function num_readable( num ) {
     var num = num + "";
     return num.slice(-9, -6) + " " + num.slice(-6, -3) + " " + num.slice(-3);
+}
+
+function initial() {
+
+    var text = "";
+
+    print_select();
+
+    if ( document.location.hash != "" ){
+        show_range_for_url();
+        }
+    else {
+        print_symbols ( 0, step, "page" );
+        text = "0  - " + num_readable( step );
+        title_symbols.innerHTML = text;
+        }
+
+    add_action_to_rangelist();
 }
 
 function print_select(){
@@ -56,6 +69,7 @@ function print_select(){
 }
 
 function print_symbols ( min, max, context ) {
+
     var out = "";
 
     var placeOutput = document.getElementById("symbols");
@@ -72,9 +86,15 @@ function print_symbols ( min, max, context ) {
         out += "<i class=\"hex\">\\" + num_to_hex( i ) + "</i></code>";
         out += "<span>"  + String.fromCharCode(i) + "</span></li>\n"; 
 
-    	out += ( i < max 
+    	if ( i < max 
                  && i > 0 
-                 && i % 1000 == 0 ) ? "</ul>\n<h2>" + i + "</h2><ul class=\"list--entities\">" : "";
+                 && i % 1000 == 0 ) {
+            if ( context == "page" ){    
+                out += "</ul>\n";
+                out += "<h2>" + i + "</h2>";
+                out += "<ul class=\"list--entities\">";
+                }
+        }
     }
 
     out += "</ul>";
@@ -84,6 +104,7 @@ function print_symbols ( min, max, context ) {
 }
 
 function remove_current() {
+
     var current_item = document.querySelector(".current");
 
     if ( current_item ) {
@@ -91,23 +112,69 @@ function remove_current() {
         }
 }
 
+function change_href ( elem ) {
+
+    var data_range = elem.getAttribute('data-range');
+    var text = elem.textContent;
+    
+    var current_href = document.location.href; 
+    var href_arr = current_href.split("#");
+
+    document.location.href = href_arr[0] + "#" + data_range + " " + text; 
+}
+
+function get_range_from_string ( data_range ) {
+    
+    var range = data_range.split(separator);
+    var min = hex_to_num( range[0] );
+    var max = hex_to_num( range[1] );
+    return [ min, max ];
+}
+
+function show_range_for_elem ( elem ){
+    
+    var data_range = elem.getAttribute('data-range');
+    var range = get_range_from_string ( data_range );
+    var min = range[0];
+    var max = range[1];
+    var text = elem.textContent;
+    
+    print_symbols ( min, max, "range" );
+    title_symbols.innerHTML = text;
+    remove_current();
+    elem.classList.add('current');
+    window.scrollTo(0,0);
+
+    change_href ( elem );
+}
+
+function show_range_for_url () {
+
+    var text = "";
+    var hash = document.location.hash.substr(1);
+    var hash_arr = hash.split(" ");
+    var data_range = hash_arr[0];
+
+    var range = get_range_from_string ( data_range );
+    var min = range[0];
+    var max = range[1];
+    print_symbols ( min, max, "href" );
+
+    for ( var i = 1; i < hash_arr.length; i++ ){
+        text += hash_arr[i] + " ";
+        }
+    title_symbols.innerHTML = text; 
+}
+
 function add_action_to_rangelist () {
-    var separator = ".."
+
     var list_items = document.querySelectorAll(".list--blocks LI");
     
     for ( var i = 0; i < list_items.length; i++ ){
         var item = list_items[i];
 
         item.addEventListener('click', function() {
-            var range = this.getAttribute('data-range').split(separator);
-            var min = hex_to_num( range[0] );
-            var max = hex_to_num( range[1] );
-            var text = this.textContent;
-
-            print_symbols ( min, max, "range" );
-            title_symbols.innerHTML = text;
-            remove_current();
-            this.classList.add('current');
+            show_range_for_elem ( this )
             }, false);
     }
 }
