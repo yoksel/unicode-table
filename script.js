d701697b5ecb407d652b984fe8de3954b3550aad
yoksel <yoksel@yandex.ru>
2014-01-01 15:42:00 +0400

    Change navigation

--- script.js
+++ script.js
@@ -1,9 +1,113 @@
 var separator = "..";
 var step = 10000;
 var title_symbols = document.querySelector(".title--symbols");
+var context = "";
 
 
 initial();
+
+function print_symbols ( min, max ) {
+
+    var out = "";
+    var placeOutput = document.getElementById("symbols");
+    placeOutput.innerHTML = "Загрузка";
+
+    min = ( min >= 0 ) ? min : max - step + 1;
+
+    out += "<ul class=\"list--entities\">";
+
+    for ( var i = min; i <= max; i++ ){
+    	var char_i = String.fromCharCode(i);
+
+        out += "<li><code><i class=\"num\">&amp;#" + i + "</i>";
+        out += "<i class=\"hex\">\\" + num_to_hex(i) + "</i></code>";
+        out += "<span>"  + char_i + "</span>";
+        out += "</li>\n"; 
+
+        if ( i < max 
+                 && i > 0 
+                 && i % 1000 == 0 ) {
+            if ( context == "page" ){    
+                out += "</ul>\n";
+                out += "<h2>" + i + "</h2>";
+                out += "<ul class=\"list--entities\">";
+                }
+        }
+    }
+
+    out += "</ul>";
+
+    placeOutput.innerHTML = out;
+}
+
+function change_data ( data_range, text ) {
+
+    var range = get_range_from_string ( data_range );
+    var min = range[0];
+    var max = range[1];
+
+    print_symbols ( min, max );
+    title_symbols.innerHTML = text + "<span>" + data_range + "</span>";
+    change_href ( data_range, text );
+    remove_current();
+    highlight_current ( data_range );
+}
+
+function add_action_to_range_select () {
+
+    var select = document.querySelector(".select--blocks");
+
+    select.addEventListener('change', function() {
+        var elem = this.options[this.selectedIndex];
+        var data_range = elem.value;
+        var text = elem.text;
+        context = "select";
+        
+        change_data ( data_range, text );
+    });
+
+}
+
+function show_range_for_elem ( elem ){
+    
+    var data_range = elem.getAttribute('data-range');
+    var text = elem.textContent;
+    context = "list";
+    
+    change_data ( data_range, text );
+    elem.classList.add('current');
+    
+}
+
+function add_action_to_range_list () {
+
+    var list_items = document.querySelectorAll(".list--blocks LI");
+    
+    for ( var i = 0; i < list_items.length; i++ ){
+        var item = list_items[i];
+
+        item.addEventListener('click', function() {
+            show_range_for_elem ( this );
+            }, false);
+    }
+}
+
+function show_range_for_url () {
+
+    var text = "";
+    context = "href";
+    var hash = document.location.hash.substr(1);
+    var hash_arr = hash.split(" ");
+    var data_range = hash_arr[0];
+
+    for ( var i = 1; i < hash_arr.length; i++ ){
+        text += hash_arr[i] + " ";
+        }
+    
+    change_data ( data_range, text );
+}
+
+//___________ Common ___________
 
 function num_to_hex ( num ){
     return num.toString(16);
@@ -22,8 +126,6 @@ function initial() {
 
     var text = "";
 
-    print_select();
-
     if ( document.location.hash != "" ){
         show_range_for_url();
         }
@@ -33,90 +135,55 @@ function initial() {
         title_symbols.innerHTML = text;
         }
 
-    add_action_to_rangelist();
+    add_action_to_range_select();    
+    add_action_to_range_list();
 }
 
-function print_select(){
+function highlight_current_list_item ( data_range ) {
+    var list_items = document.querySelectorAll(".list--blocks LI");
 
-    var placeOutput = document.getElementById("wrapper--range-select");
+    for (var i = 0; i < list_items.length; i++ ){
+        var elem = list_items[i];
+        var data_range_attr = elem.getAttribute("data-range");
 
-    var options = "";
-    var max = 100; //1112064; // http://ru.wikipedia.org/wiki/%D0%AE%D0%BD%D0%B8%D0%BA%D0%BE%D0%B4
-    
-    for ( var i = 0; i <= max; i++ ){
-        var step_min = ( i > 0) ? i * step + 1 : i * step;
-        var step_max = (i + 1) * step;
-        var step_output = num_readable( step_min ) + " - " + num_readable( step_max );
-        options += "<option value=\"" + step_max + "\">" + step_output + "</option>"
-    }
-
-    options = "Диапазон: <select id=\"range_select\">" + options + "</select>"; //onchange=\"print_symbols()\"
-
-    placeOutput.innerHTML = options;
-
-    var myselect = document.getElementById("range_select");
-    myselect.addEventListener('change', function() {
-            var select_val = this.options[this.selectedIndex].value;
-            var max = +select_val;
-            var min = max - step + 1;
-            var text = num_readable( min ) + " - " + num_readable( max );
-
-            title_symbols.innerHTML = text;
-            print_symbols ( min, max );
-            remove_current();
-            }, false);
-
-}
-
-function print_symbols ( min, max, context ) {
-
-    var out = "";
-
-    var placeOutput = document.getElementById("symbols");
-    placeOutput.innerHTML = "Загрузка";
-
-    min = min || max - step + 1;
-
-    out += "<ul class=\"list--entities\">";
-
-    for ( var i = min; i <= max; i++ ){
-    	var char_i = String.fromCharCode(i);
-
-        out += "<li><code><i class=\"num\">&amp;#" + i + "</i>";
-        out += "<i class=\"hex\">\\" + num_to_hex( i ) + "</i></code>";
-        out += "<span>"  + String.fromCharCode(i) + "</span></li>\n"; 
-
-    	if ( i < max 
-                 && i > 0 
-                 && i % 1000 == 0 ) {
-            if ( context == "page" ){    
-                out += "</ul>\n";
-                out += "<h2>" + i + "</h2>";
-                out += "<ul class=\"list--entities\">";
-                }
+        if ( data_range_attr == data_range ) {
+            elem.classList.add("current");
+            }
         }
-    }
+}
+function highlight_current_option ( data_range ) {
+    var list_items = document.querySelectorAll(".select--blocks option");
 
-    out += "</ul>";
+    for (var i = 0; i < list_items.length; i++ ){
+        var elem = list_items[i];
+        var data_range_attr = elem.value;
 
-    placeOutput.innerHTML = out;
-
+        if ( data_range_attr == data_range ) {
+            elem.selected = true;
+            }
+        }
 }
 
-function remove_current() {
+function highlight_current( data_range ) {
+    if ( context != "list") {
+        highlight_current_list_item ( data_range );
+        }
+    if ( context != "select" ){
+        highlight_current_option ( data_range );
+        }    
+}
+
+function remove_current () {
 
     var current_item = document.querySelector(".current");
 
     if ( current_item ) {
-        current_item.classList.remove('current');
+        current_item.classList.remove("current");
         }
 }
 
-function change_href ( elem ) {
+function change_href ( data_range, text ) {
 
-    var data_range = elem.getAttribute('data-range');
-    var text = elem.textContent;
-    
     var current_href = document.location.href; 
     var href_arr = current_href.split("#");
 
@@ -129,52 +196,4 @@ function get_range_from_string ( data_range ) {
     var min = hex_to_num( range[0] );
     var max = hex_to_num( range[1] );
     return [ min, max ];
-}
-
-function show_range_for_elem ( elem ){
-    
-    var data_range = elem.getAttribute('data-range');
-    var range = get_range_from_string ( data_range );
-    var min = range[0];
-    var max = range[1];
-    var text = elem.textContent;
-    
-    print_symbols ( min, max, "range" );
-    title_symbols.innerHTML = text;
-    remove_current();
-    elem.classList.add('current');
-    window.scrollTo(0,0);
-
-    change_href ( elem );
-}
-
-function show_range_for_url () {
-
-    var text = "";
-    var hash = document.location.hash.substr(1);
-    var hash_arr = hash.split(" ");
-    var data_range = hash_arr[0];
-
-    var range = get_range_from_string ( data_range );
-    var min = range[0];
-    var max = range[1];
-    print_symbols ( min, max, "href" );
-
-    for ( var i = 1; i < hash_arr.length; i++ ){
-        text += hash_arr[i] + " ";
-        }
-    title_symbols.innerHTML = text; 
-}
-
-function add_action_to_rangelist () {
-
-    var list_items = document.querySelectorAll(".list--blocks LI");
-    
-    for ( var i = 0; i < list_items.length; i++ ){
-        var item = list_items[i];
-
-        item.addEventListener('click', function() {
-            show_range_for_elem ( this )
-            }, false);
-    }
 }
