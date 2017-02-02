740357fb1f706f6a6542c1676eeb4a8d63138f81
yokselzok <yokselzok@gmail.com>
2017-02-02 17:30:10 +0300

    Some updates

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 var separator = "..";
 var step = 10000;
-var title_symbols = document.querySelector(".title--symbols");
+var content__title = document.querySelector(".content__title");
 var context = "";
 var fonts = ["Arial", "Arial Unicode MS", "Courier New", "Georgia", "Tahoma", "Times", "Trebuchet", "Verdana"];
 var default_title = "Полезные символы<span>Моя подборка</span>";
@@ -13,12 +13,12 @@ function print_symbols ( min, max ) {
     var placeOutput = document.getElementById("symbols");
     placeOutput.innerHTML = "Загрузка";
 
-    out += "<ul class=\"list--entities\">";
+    out += "<ul class=\"entities\">";
 
     if ( max > 0 ){
         min = ( min >= 0 ) ? min : max - step + 1;
         for ( var i = min; i <= max; i++ ){
-        	
+
             out += get_symbol (i, max);
         }
     }
@@ -43,7 +43,7 @@ function change_data ( data_range, text ) {
     var max = range[1];
 
     print_symbols ( min, max );
-    title_symbols.innerHTML = text + "<span>" + data_range + "</span>";
+    content__title.innerHTML = text + "<span>" + data_range + "</span>";
     change_href ( data_range, text );
     remove_class("current");
     highlight_current ( data_range );
@@ -51,34 +51,34 @@ function change_data ( data_range, text ) {
 
 function add_action_to_range_select () {
 
-    var select = document.querySelector(".select--blocks");
+    var select = document.querySelector(".content__select");
 
     select.addEventListener('change', function() {
         var elem = this.options[this.selectedIndex];
         var data_range = elem.value;
         var text = elem.text;
         context = "select";
-        
+
         change_data ( data_range, text );
     });
 
 }
 
 function show_range_for_elem ( elem ){
-    
+
     var data_range = elem.getAttribute('data-range');
     var text = elem.textContent;
     context = "list";
-    
+
     change_data ( data_range, text );
     elem.classList.add('current');
-    
+
 }
 
 function add_action_to_range_list () {
 
     var list_items = document.querySelectorAll(".list--blocks LI");
-    
+
     for ( var i = 0; i < list_items.length; i++ ){
         var item = list_items[i];
 
@@ -109,17 +109,17 @@ function show_range_for_url () {
             text += " ";
             }
         }
-    
+
     change_data ( data_range, text );
 }
 
 function add_action_to_symbols_list () {
     var chars = document.querySelectorAll(".show-case");
-    var class_name = "full-width";
+    var class_name = "entities__item--wide";
 
     for ( var i = 0; i < chars.length; i++ ){
         var chars_item = chars[i];
-        
+
         chars_item.addEventListener ('click', function() {
 
             var parent = this.parentNode;
@@ -143,18 +143,18 @@ function create_fonts_list ( char_item ) {
         var font = fonts[k];
         var font_class = font.toLowerCase(font);
         font_class = "f-" + string_to_class ( font_class );
-        
+
         show_case_content += "<li class=\"font-item " + font_class + "\" data-name=\"" + font + "\">";
         show_case_content += "<span class=\"font-name\">" + font + "</span>";
         show_case_content += "<span class=\"font-demo\">" + char_item + "</span>";
         show_case_content += "</li>";
         }
 
-    return show_case_content;  
+    return show_case_content;
 }
 
 function add_action_to_close () {
-    
+
     var elems = document.querySelectorAll(".close");
 
     for ( var i = 0; i < elems.length; i++ ){
@@ -166,9 +166,9 @@ function add_action_to_close () {
 
 function close_show_case () {
 
-    var class_name = "full-width";
-    var show_case = document.querySelector(".full-width .show-case");
-    
+    var class_name = "entities__item--wide";
+    var show_case = document.querySelector(".entities__item--wide .show-case");
+
     if ( show_case ){
         var show_case_content = show_case.getAttribute("data-char");
         show_case.innerHTML = show_case_content;
@@ -198,10 +198,10 @@ function initial() {
         }
     else {
         print_symbols ( 0, 0 );
-        title_symbols.innerHTML = default_title;
+        content__title.innerHTML = default_title;
         }
 
-    add_action_to_range_select();    
+    add_action_to_range_select();
     add_action_to_range_list();
     add_action_to_close ();
 }
@@ -210,15 +210,15 @@ function get_symbol( i, max ) {
     var out = "";
     var char_i = String.fromCharCode(i);
 
-    out += "<li class=\"item--entities\"><code><i class=\"num\">&amp;#" + i + "</i>";
+    out += "<li class=\"entities__item\"><code><i class=\"num\">&amp;#" + i + "</i>";
     out += "<i class=\"hex\">\\" + num_to_hex(i) + "</i></code>";
     out += "<ul class=\"show-case\" data-char=\"" + char_i + "\"><li>"  + char_i + "</li></ul>";
-    out += "<span class=\"close\"></span>"; 
-    out += "</li>\n"; 
+    out += "<span class=\"close\"></span>";
+    out += "</li>\n";
 
-    if ( i < max 
-             && i > 0 
-             && i % 1000 == 0 
+    if ( i < max
+             && i > 0
+             && i % 1000 == 0
              && context == "page" ) {
                 out += "</ul>\n";
                 out += "<h2>" + i + "</h2>";
@@ -241,7 +241,7 @@ function highlight_current_list_item ( data_range ) {
         }
 }
 function highlight_current_option ( data_range ) {
-    var list_items = document.querySelectorAll(".select--blocks option");
+    var list_items = document.querySelectorAll(".content__select option");
 
     for (var i = 0; i < list_items.length; i++ ){
         var elem = list_items[i];
@@ -259,7 +259,7 @@ function highlight_current( data_range ) {
         }
     if ( context != "select" ){
         highlight_current_option ( data_range );
-        }    
+        }
 }
 
 function remove_class ( class_name ) {
@@ -273,14 +273,14 @@ function remove_class ( class_name ) {
 
 function change_href ( data_range, text ) {
 
-    var current_href = document.location.href; 
+    var current_href = document.location.href;
     var href_arr = current_href.split("#");
 
-    document.location.href = href_arr[0] + "#" + data_range + " " + text; 
+    document.location.href = href_arr[0] + "#" + data_range + " " + text;
 }
 
 function get_range_from_string ( data_range ) {
-    
+
     var range = data_range.split(separator);
     var min = hex_to_num( range[0] );
     var max = hex_to_num( range[1] );
